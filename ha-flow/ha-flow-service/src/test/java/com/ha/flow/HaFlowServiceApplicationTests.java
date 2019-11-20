87bb11ea88534d364cc81231a974ceb344dbd15d
dev_mlzhang <dev_mlzhang@163.com>
2019-11-20 11:06:43 +0800

    新增工作流模块// TODO

---
+++ ha-flow/ha-flow-service/src/test/java/com/ha/flow/HaFlowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ha.flow;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaFlowServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
