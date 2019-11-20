f74e824f7f382634efe8b218667116ff2fbd8dbb
dev_mlzhang <dev_mlzhang@163.com>
2019-11-20 10:59:31 +0800

    新增工作流模块// TODO

---
+++ ha-flow/ha-flow-interface/src/main/java/com/ha/flow/api/TestApi.java
@@ -0,0 +1,11 @@
+package com.ha.flow.api;
+/**
+ * <p>
+ *   测试
+ * </p>
+ *
+ * @author ML.Zhang
+ * @since  2019/11/20
+ */
+public class TestApi {
+}
---
+++ ha-flow/ha-flow-interface/src/main/java/com/ha/flow/pojo/TestPojo.java
@@ -0,0 +1,12 @@
+package com.ha.flow.pojo;
+
+/**
+ * <p>
+ * 测试
+ * </p>
+ *
+ * @author ML.Zhang
+ * @since 2019/11/20
+ */
+public class TestPojo {
+}
