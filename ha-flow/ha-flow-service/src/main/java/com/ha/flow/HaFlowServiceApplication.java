0351237841e6f1cf32eea2aea2681a6d64a70453
dev_mlzhang <dev_mlzhang@163.com>
2019-11-20 11:05:44 +0800

    新增工作流模块// TODO

---
+++ ha-flow/ha-flow-service/src/main/java/com/ha/flow/HaFlowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ha.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaFlowServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaFlowServiceApplication.class, args);
+    }
+
+}
