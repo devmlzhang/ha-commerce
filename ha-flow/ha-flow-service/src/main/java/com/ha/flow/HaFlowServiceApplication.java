a7d74728611760573076b1c6c7b27597a5c4b5e4
dev_mlzhang <dev_mlzhang@163.com>
2019-11-20 18:22:22 +0800

    新增工作流模块// TODO

--- ha-flow/ha-flow-service/src/main/java/com/ha/flow/HaFlowServiceApplication.java
+++ ha-flow/ha-flow-service/src/main/java/com/ha/flow/HaFlowServiceApplication.java
@@ -2,8 +2,14 @@ package com.ha.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@MapperScan("com.ha.flow.mapper")
 public class HaFlowServiceApplication {
 
     public static void main(String[] args) {
--- ha-flow/ha-flow-service/src/test/java/com/ha/flow/HaFlowServiceApplicationTests.java
+++ ha-flow/ha-flow-service/src/test/java/com/ha/flow/HaFlowServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.ha.flow;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * <p>
+ *  测试类
+ * </p>
+ *
+ * @author ML.Zhang
+ * @since 2019/11/20
+ */
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class HaFlowServiceApplicationTests {
+public class HaFlowServiceApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    public void name() {
 
+    }
 }
